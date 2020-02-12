3e82aabc4fa53bd1a5d6c854e5a28f68ad2ce5e6
Bismo Baruno <bismobaruno@gmail.com>
2020-02-12 12:36:22 +0700

    adding delivery test for list and update

--- api/v1/user/delivery_test.go
+++ api/v1/user/delivery_test.go
@@ -7,11 +7,13 @@
 package user_test
 
 import (
+	"github.com/moemoe89/simple-go-clean-arch/api/v1/api_struct/form"
+	"github.com/moemoe89/simple-go-clean-arch/api/v1/api_struct/model"
 	"github.com/moemoe89/simple-go-clean-arch/api/v1/user/mocks"
 	"github.com/moemoe89/simple-go-clean-arch/config"
-	"github.com/moemoe89/simple-go-clean-arch/api/v1/api_struct/model"
 	"github.com/moemoe89/simple-go-clean-arch/routers"
 
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -21,6 +23,81 @@ import (
 	"github.com/rs/xid"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestDeliveryUpadte(t *testing.T) {
+	id := xid.New().String()
+
+	lang, _ := config.InitLang()
+	log := config.InitLog()
+
+	userForm := &form.UserForm{
+		ID:      xid.New().String(),
+		Name:    "Momo",
+		Email:   "momo@mail.com",
+		Phone:   "085640",
+		Address: "Indonesia",
+	}
+	user := &model.UserModel{
+		ID:      userForm.ID,
+		Name:    userForm.Name,
+		Email:   userForm.Email,
+		Phone:   userForm.Phone,
+		Address: userForm.Address,
+	}
+
+	j, err := json.Marshal(userForm)
+	assert.NoError(t, err)
+
+	mockService := new(mocks.Service)
+	mockService.On("Detail", id, "id").Return(user, 0, nil)
+	mockService.On("Update", userForm, id).Return(user, 0, nil)
+
+	router:= routers.GetRouter(lang, log, mockService)
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest("PUT", "/api/v1/user/"+id, strings.NewReader(string(j)))
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotNil(t, w.Body)
+}
+
+func TestDeliveryList(t *testing.T) {
+	lang, _ := config.InitLang()
+	log := config.InitLog()
+
+	user := &model.UserModel{
+		ID:      xid.New().String(),
+		Name:    "Momo",
+		Email:   "momo@mail.com",
+		Phone:   "085640",
+		Address: "Indonesia",
+	}
+	users := []*model.UserModel{}
+	users = append(users, user)
+
+	filter := map[string]interface{}{}
+	filter["name"] = "%a%"
+	filter["email"] = "%b%"
+	filter["phone"] = "%0856%"
+	filterCount := filter
+	filter["limit"] = 10
+	filter["offset"] = 0
+
+	mockService := new(mocks.Service)
+	mockService.On("List", filter, filterCount, "WHERE deleted_at IS NULL AND name LIKE :name AND email LIKE :email AND phone LIKE :phone", "created_at DESC", model.UserSelectField).Return(users, 1, 0, nil)
+
+	router:= routers.GetRouter(lang, log, mockService)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/user?per_page=10&name=a&email=b&phone=0856", strings.NewReader(""))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotNil(t, w.Body)
+}
 
 func TestDeliveryDetail(t *testing.T) {
 	id := xid.New().String()
